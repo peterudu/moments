2a651b9706509446e9ff6cceebffd46f8966e371
peterudu <puduevborho7@gmail.com>
2023-12-05 22:59:12 +0000

    Create SignUp form

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://djangoframework-api-7834ad396166.herokuapp.com/';
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
+axios.defaults.withCredentials = true;
